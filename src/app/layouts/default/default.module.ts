eb5b83f9d25ae9e6220b0059ae82e3e9dd2dbd0b
Wild <cadainmiller@gmail.com>
2020-07-25 21:15:43 -0500

    Created A dashboard with angular

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+
+import { DefaultComponent } from '../default/default.component';
+import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
+import { PostComponent } from '../../modules/post/post.component';
+import { SharedModule } from '../../shared/shared.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { DashboardService } from '../../services/dashboard.service';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatSortModule,
+    MatTableModule,
+    MatBadgeModule
+  ],
+  providers: [
+    DashboardService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class DefaultModule { }
---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { DashboardService } from '../../services/dashboard.service'
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  bigChart = [];
+  cards = [];
+  pieChart = [];
+
+  constructor(private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+    this.bigChart = this.dashboardService.bigChart();
+    this.cards = this.dashboardService.cards();
+    this.pieChart = this.dashboardService.pieChart();
+
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+
+];
